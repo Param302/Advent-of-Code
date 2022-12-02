e16afecbedc3a9e98814bb602c0397a3047b1d81
Parampreet Singh <rubercuber.30@gmail.com>
2022-12-03 01:14:13 +0530

    Advent-of-Code 2022 Day 2 done

--- 2022/main.py
+++ 2022/main.py
@@ -1,21 +1,82 @@
-with open("./inputs/day1.txt") as f:
-    raw_data = f.readlines()
-# adding extra newline to include last calorie
-raw_data.append("\n")
+with open("./inputs/day2.txt") as f:
+    all_rounds = f.readlines()
 
-data = []
-s = 0
-for i in raw_data:
-    if i!="\n":
-        s += int(i[:-1])
+scores = {
+    "X": 1,
+    "Y": 2,
+    "Z": 3
+}
+
+correct_choices = {
+    "A": "Y",
+    "B": "Z",
+    "C": "X"
+}
+
+wrong_choices = {
+    "A":"Z",
+    "B":"X",
+    "C":"Y"
+}
+
+
+draws = {
+    "A": "X",     # rock
+    "B": "Y",     # paper
+    "C": "Z"      # scissors
+}
+
+tweaked_scores = {
+    "X": 0,
+    "Y": 3,
+    "Z": 6
+}
+
+
+"""
+0 -> lost
+3 -> draw
+6 -> won
+
+
+A Y
+B X
+C Z
+
+"""
+
+# Part 2
+score = 0
+for round in all_rounds:
+    opponent, mine = round.split()
+
+    if mine=="Y":
+        choose = draws[opponent]
+    elif mine=="Z":
+        choose = correct_choices[opponent]
     else:
-        data.append(s)
-        s = 0
+        choose = wrong_choices[opponent]
 
-data.sort(reverse=True)
+    # print("opponent", opponent)
+    # print("What to do?", mine)
+    # print("Choose", choose)
+    # print("tweaked_scores", tweaked_scores[mine])
+    # print("score of choosing", scores[choose])
+    score += tweaked_scores[mine] + scores[choose]
+    # print(score)
 
-# 1st part: Highest no. of calories carried by an Elf
-print(f"1st part solution - {data[0]}")
+print(score)
 
-# 2nd part: Total calories carried by top 3 Elves
-print(f"2nd part solution - {sum(data[:3])}")
+# Part 1
+score = 0
+for round in all_rounds:
+    opponent, mine = round.split()
+
+    if draws[opponent]==mine:
+        score += 3 + scores[mine]
+    elif correct_choices[opponent] == mine:
+        score += 6 + scores[mine]
+    else:
+        score += scores[mine]
+
+print(score)

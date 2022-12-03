5ad9e1c44a8e151f65196e7ffc3c3bf7074d3ea8
Parampreet Singh <rubercuber.30@gmail.com>
2022-12-04 00:35:14 +0530

    Advent-of-Code 2022 Day 3 done

--- 2022/main.py
+++ 2022/main.py
@@ -1,82 +1,33 @@
-with open("./inputs/day2.txt") as f:
-    all_rounds = f.readlines()
+with open("./inputs/day3.txt") as f:
+    all_rucksacks = [i.strip() for i in f]
 
-scores = {
-    "X": 1,
-    "Y": 2,
-    "Z": 3
-}
-
-correct_choices = {
-    "A": "Y",
-    "B": "Z",
-    "C": "X"
-}
-
-wrong_choices = {
-    "A":"Z",
-    "B":"X",
-    "C":"Y"
-}
-
-
-draws = {
-    "A": "X",     # rock
-    "B": "Y",     # paper
-    "C": "Z"      # scissors
-}
-
-tweaked_scores = {
-    "X": 0,
-    "Y": 3,
-    "Z": 6
-}
-
-
-"""
-0 -> lost
-3 -> draw
-6 -> won
-
-
-A Y
-B X
-C Z
-
-"""
-
-# Part 2
-score = 0
-for round in all_rounds:
-    opponent, mine = round.split()
-
-    if mine=="Y":
-        choose = draws[opponent]
-    elif mine=="Z":
-        choose = correct_choices[opponent]
-    else:
-        choose = wrong_choices[opponent]
-
-    # print("opponent", opponent)
-    # print("What to do?", mine)
-    # print("Choose", choose)
-    # print("tweaked_scores", tweaked_scores[mine])
-    # print("score of choosing", scores[choose])
-    score += tweaked_scores[mine] + scores[choose]
-    # print(score)
-
-print(score)
+def map_lowercase_items(item): return ord(item)-96
+def map_uppercase_items(item): return ord(item) % 65 + 27
 
 # Part 1
-score = 0
-for round in all_rounds:
-    opponent, mine = round.split()
+sump = 0
+for rucksack in all_rucksacks:
+    rucksack = rucksack.strip()
+    n = len(rucksack)
+    first, second = set(rucksack[:n//2]), set(rucksack[n//2:])  # compartments
+    common_item = first.intersection(second).pop()
+    # print(x)
+    sump += map_lowercase_items(common_item) if common_item.islower() else map_uppercase_items(common_item)
+print(sump)
 
-    if draws[opponent]==mine:
-        score += 3 + scores[mine]
-    elif correct_choices[opponent] == mine:
-        score += 6 + scores[mine]
-    else:
-        score += scores[mine]
 
-print(score)
+# Part 2
+sump = 0
+for ind, rucksack in enumerate(all_rucksacks, 1):
+    if ind % 3 == 0:    # get each group
+        # print(f"Index: {ind}")
+        first_rs = set(all_rucksacks[ind-3])
+        sec_rs = set(all_rucksacks[ind-2])
+        third_rs = set(all_rucksacks[ind-1])
+        sec_rs.intersection_update(third_rs)
+        common_item = first_rs.intersection(sec_rs).pop()
+        sump += map_lowercase_items(common_item) if common_item.islower(
+        ) else map_uppercase_items(common_item)
+print(sump)
+
+

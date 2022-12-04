17a1d6fb7f544015c6aec421e67b5599e308f086
Parampreet Singh <rubercuber.30@gmail.com>
2022-12-04 23:44:57 +0530

    Advent-of-Code 2022 Day 4 done

--- 2022/main.py
+++ 2022/main.py
@@ -1,33 +1,31 @@
-with open("./inputs/day3.txt") as f:
-    all_rucksacks = [i.strip() for i in f]
-
-def map_lowercase_items(item): return ord(item)-96
-def map_uppercase_items(item): return ord(item) % 65 + 27
+with open("./inputs/day4.txt") as f:
+    assignment_pairs = [
+        [
+            set(range(int(i.split('-')[0]), int(i.split('-')[1])+1))
+            for i in pair.split(',')
+        ] 
+        for pair in f
+        ]
 
 # Part 1
-sump = 0
-for rucksack in all_rucksacks:
-    rucksack = rucksack.strip()
-    n = len(rucksack)
-    first, second = set(rucksack[:n//2]), set(rucksack[n//2:])  # compartments
-    common_item = first.intersection(second).pop()
-    # print(x)
-    sump += map_lowercase_items(common_item) if common_item.islower() else map_uppercase_items(common_item)
-print(sump)
+n_fully_contains = 0
+for i in assignment_pairs:
+    pair1, pair2 = i
+    if pair1.issubset(pair2) or pair2.issubset(pair1):
+        n_fully_contains += 1
+
+print(n_fully_contains)
 
 
 # Part 2
-sump = 0
-for ind, rucksack in enumerate(all_rucksacks, 1):
-    if ind % 3 == 0:    # get each group
-        # print(f"Index: {ind}")
-        first_rs = set(all_rucksacks[ind-3])
-        sec_rs = set(all_rucksacks[ind-2])
-        third_rs = set(all_rucksacks[ind-1])
-        sec_rs.intersection_update(third_rs)
-        common_item = first_rs.intersection(sec_rs).pop()
-        sump += map_lowercase_items(common_item) if common_item.islower(
-        ) else map_uppercase_items(common_item)
-print(sump)
+def is_overlap(pair1, pair2):
+    p1_in_p2 = any(i in pair2 for i in pair1)
+    p2_in_p1 = any(i in pair1 for i in pair2)
+    return p1_in_p2 and p2_in_p1
 
-
+n_overlaps = 0
+for i in assignment_pairs:
+    pair1, pair2 = i
+    if is_overlap(pair1, pair2):
+        n_overlaps += 1
+print(n_overlaps)
